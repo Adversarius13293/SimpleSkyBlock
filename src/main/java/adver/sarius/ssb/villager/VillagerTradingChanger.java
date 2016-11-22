b0e83fb24d002f76c8a7e1273e5e5a5b6f673ed9
Adversarius <pierre13293@yahoo.de>
2016-11-22 16:27:26 +0100

    Removed grass trade.

--- src/main/java/adver/sarius/ssb/villager/VillagerTradingChanger.java
+++ src/main/java/adver/sarius/ssb/villager/VillagerTradingChanger.java
@@ -25,7 +25,6 @@ public class VillagerTradingChanger {
 
 		VillagerRegistry.instance().register(prof);
 		ITradeList tradeSugar = new EntityVillager.ItemAndEmeraldToItem(Items.SUGAR, new PriceInfo(5, 8), Items.REEDS, new PriceInfo(1, 1));
-//		ITradeList tradeCactus = new EntityVillager.ListItemForEmeralds(Item.getItemFromBlock(Blocks.CACTUS), new PriceInfo(8, 12));
 		ITradeList tradeCactus = new ItemAndItemToItem(new ItemStack(Blocks.CACTUS), new PriceInfo(1, 1), 
 				new ItemStack(Items.FISH, 1, 3), new PriceInfo(1, 1), new ItemStack(Items.EMERALD), new PriceInfo(4, 6));
 		ITradeList tradeCocoa = new ItemAndItemToItem(new ItemStack(Items.DYE, 1, 3), new PriceInfo(1, 1), 
@@ -55,6 +54,6 @@ public class VillagerTradingChanger {
 		career.addTrade(1, tradeSugar, tradeCactus, tradeCocoa);
 		career.addTrade(2, tradeSpruce, tradeBirch, tradeJungle, tradeAcacia, tradeDark);
 		career.addTrade(3, tradeSun, tradeLilac, tradeRose, tradePeony);
-		career.addTrade(4, tradeGrass);
+//		career.addTrade(4, tradeGrass);
 	}
 }
